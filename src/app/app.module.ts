63c5ace13008a880724935d3faf8e231b89b3fa3
Tanusree <tanusree@capitalnumbers.com>
2020-02-07 12:09:21 +0530

    Changes done for Warning Assignment Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {SuccessComponent} from './successAlert/success.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SuccessComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule
